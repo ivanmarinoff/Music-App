d152045536940b8a5ec82a3488fda2d1a25443c3
ivanmarinoff <marinoffivan@gmail.com>
2023-12-09 18:32:48 +0200

    Update gitignore

--- MusicApp/settings.py
+++ MusicApp/settings.py
@@ -99,7 +99,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
-    BASE_DIR / "MusicApp/staticfiles",
+    BASE_DIR / "static",
 ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
